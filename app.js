7b8c7041472739ecb89e4a46ff9323a7e1268976
ashleyyz068 <144456040+ashleyyz068@users.noreply.github.com>
2023-10-04 09:24:47 -0400

    wed

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-const num1 =7;
-const num2 =8;
-let add = num1 +num2;// replace the numbers with varibales 
-
-const str= "hello"
-const name ="my name"
-const realName = "is ashley"
-const literal = `${str} ${name} ${realName}` // alternative for concatiation
-console.log(literal)
+let a = Number(prompt("enert number"))
+let b = Number(prompt("enert number"))
+function BMI(){
+    return(a/b);
+}
+console.log(BMI())
