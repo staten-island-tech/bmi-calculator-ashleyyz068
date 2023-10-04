c440074c1c610cc2618490effd457a350176eb1d
ashleyyz068 <144456040+ashleyyz068@users.noreply.github.com>
2023-10-04 08:51:26 -0400

    doc

--- app.js
+++ app.js
@@ -1,37 +1,9 @@
-// // pass in an arguement or parameters 
-// // dclare and define function
-// // declared and never read means that you didn't do anything 
-// function test(mess){
-//     console.log(mess)
-// }
-// test(true);//string; passing something into a function has to be a string or valiad data tupe 
-// const x ="string"; // declaring and defining a variable
-// test(x)
-// // int
-// const y = 98;
-// test(y);
-// // boolean t or f
-// const z =true; 
-// test(z);
+const num1 =7;
+const num2 =8;
+let add = num1 +num2;// replace the numbers with varibales 
 
-// function add(a,b){
-//     console.log(a+b)
-// }
-// add(y,33)
-
-// let a = Number(prompt("enert number"))
-// let b = Number(prompt("enert another number"))
-
-// add(a,b)
-function add(x,y){
-    //create an output
-    return x+y
-}
-const sum= add(45,34)
-console.log(sum);
-
-function calc(){
-    const sum = add(45,34)
-    console.log(sum)
-}
-calc()
+const str= "hello"
+const name ="my name"
+const realName = "is ashley"
+const literal = `${str} ${name} ${realName}` // alternative for concatiation
+console.log(literal)
