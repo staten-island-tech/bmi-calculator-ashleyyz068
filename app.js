4f80f8f1bbe95316669fb5b97d314d9c1975fb28
ashleyyz068 <144456040+ashleyyz068@users.noreply.github.com>
2023-10-05 23:48:43 -0400

    final

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-const user = {
+
+let userName = prompt("Enter your name:");
+let userWeight = Number(prompt("Enter your weight in kilograms:"));
+let userHeight = Number(prompt("Enter your height in centimeters:"));
+function BMI() {
+    return (userWeight / (userHeight / 100));
+}
+const bmi = BMI();
+
+console.log("Your BMI is " + bmi)
+
+function health(x) {
+    if (x < 18.5) {
+        console.log("You are  underweight")
+    }
+    if (x >= 18.5 && x < 24.9) {
+        console.log("You are within the healthy weight range")
+    }
+    if (x >= 24.9 && x < 29.9) {
+        console.log("You are overweight")
+    }
 
 }
-let a = Number(prompt("Enter your weight in pounds:"))
-let b = Number(prompt("Enter your height in inches"))
-function BMI(){
-    return(a/b);
-}
-console.log("Your BMI is " + BMI())
+health(bmi)
